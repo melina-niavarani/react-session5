ce38ac92aeb47def84f3903c530a95847be0f1f9
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-09-17 22:02:13 +0330

    list of arrays

--- arrays.ts
+++ arrays.ts
@@ -10,10 +10,10 @@ const names: string[]= []
 const contact: Person[]= []; 
 
 values.push(10)
-values.push("mel")
+// values.push("mel")
 
 names.push("melina")
-names.push(20)
+// names.push(20)
 
 contact.push({
     name: "foolan",
@@ -21,3 +21,32 @@ contact.push({
     age: 10,
     is_present: false,
 })
+
+interface Animal {
+    name: string,
+    breed: string
+}
+
+const dog: Animal= {
+    name: "negro",
+    breed: "german",
+}
+const wolf: Animal= {
+    name: "black",
+    breed: "asian",
+}
+const beer: Animal= {
+    name: "browny",
+    breed: "austrailian"
+}
+// list kole animals o bekhaim :
+function my_zoo(animals: Animal[]){
+    console.log(animals)
+}
+const list = [
+    dog,
+    wolf,
+    beer
+]
+
+my_zoo(list)
